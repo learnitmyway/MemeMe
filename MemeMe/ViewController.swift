cd1e95e21f86e631c265a2a81fd3f824ae8a1ab5
David Swallow <dswallow4@gmail.com>
2016-10-29 20:27:34 +0200

    Add and connect view elements, present image picker

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func presentImagePicker(_ sender: UIBarButtonItem) {
+        let imagePicker = UIImagePickerController()
+        self.present(imagePicker, animated: true, completion: nil)
+    }
 
 }
 
