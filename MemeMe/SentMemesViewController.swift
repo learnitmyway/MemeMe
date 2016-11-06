083ac34a7be34edf94363ffb47ec5c3e6e19f306
David Swallow <dswallow4@gmail.com>
2016-11-06 21:19:44 +0100

    Add Container for Collection View and activate toolbar buttons

--- MemeMe/SentMemesViewController.swift
+++ MemeMe/SentMemesViewController.swift
@@ -10,8 +10,26 @@ import UIKit
 
 class SentMemesViewController: UIViewController {
     
+    @IBOutlet weak var tableContainerView: UIView!
+    @IBOutlet weak var collectionContainerView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
 
+    @IBAction func showTableView(_ sender: UIBarButtonItem) {
+        UIView.animate(withDuration: 0.5, animations: {
+            self.tableContainerView.alpha = 1
+            self.collectionContainerView.alpha = 0
+        })
+    }
+    
+    @IBAction func showGridView(_ sender: UIBarButtonItem) {
+        UIView.animate(withDuration: 0.5, animations: {
+            self.tableContainerView.alpha = 0
+            self.collectionContainerView.alpha = 1
+        })
+    }
+    
 }
